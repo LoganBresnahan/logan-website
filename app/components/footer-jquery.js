6eae025f4562a1dea625e4ccafdb9187f1b0ff65
loganbresnahan <loganbbres@gmail.com>
2016-03-26 21:55:51 -0500

    Update front page.

--- app/components/footer-jquery.js
+++ app/components/footer-jquery.js
@@ -1,32 +1,5 @@
-this.$(window).bind("load", function() {
+import Ember from 'ember';
 
-       var footerHeight = 0,
-           footerTop = 0,
-           $footer = this.$(".page-footer");
-
-       positionFooter();
-
-       function positionFooter() {
-
-                footerHeight = $footer.height();
-                footerTop = (this.$(window).scrollTop()+this.$(window).height()-footerHeight)+"px";
-
-               if ( (this.$(document.body).height()+footerHeight) < this.$(window).height()) {
-                   $footer.css({
-                        position: "absolute"
-                   }).animate({
-                        top: footerTop
-                   });
-               } else {
-                   $footer.css({
-                        position: "static"
-                   });
-               }
-
-       }
-
-       this.$(window)
-               .scroll(positionFooter)
-               .resize(positionFooter);
-
+Ember.$(document).ready(function(){
+  Ember.$('.carousel').carousel();
 });
