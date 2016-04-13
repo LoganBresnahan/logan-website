10356b160172d22a88d2280cd7220d5520eaacc1
loganbresnahan <loganbbres@gmail.com>
2016-04-13 15:09:49 -0500

    Change from page to have a slider and comment out wheel.

--- app/components/x-slider.js
+++ app/components/x-slider.js
@@ -1,14 +1,9 @@
-// export default Ember.Component.extend({
-//   didInsertElement: function(){
-//
-//     $(document).ready(function(){
-//       $('.slider').slider({full_width: true});
-//       $('.slider').slider().css('margin-left', 25+'em');
-//       $('.slider').slider().css('width', 75+'%');
-//       // $('.indicator-item').on('click', function(){
-//         $('.indicator-item').click().closest('li').css('color', 'green');
-//       // });
-//     });
-//
-//   }
-// });
+export default Ember.Component.extend({
+  didInsertElement: function(){
+
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+  }
+});
